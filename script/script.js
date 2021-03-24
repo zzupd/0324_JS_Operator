11aa227b5a090727c46ee125597932997e1212d4
zzupd <zzupd@naver.com>
2021-03-24 13:10:08 +0900

    0324_JS&HTML_데이터 전달

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ $(function () {
         var number1 = $("#number1").val();    // .text() => 요소의 내용
         var number2 = $("#number2").val();    // .val() => 입력양식의 값
 
-        var result = parseInt(number1) + parseInt(number2);
+        var result = parseInt(number1) % parseInt(number2);
         $("#addRes").text("result : " + result);
     });
 
